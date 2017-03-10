d82c21ce0cf6fa5908d9a51255996088baac5201
Knight-ZXW <nimdanoob@163.com>
2017-03-10 16:22:26 +0800

    注意! web.xml的配置顺序是有作用的，不要随意调整顺序，如果 日志 listener 需要在 contextListener之前才能起作用

--- springworld/src/main/java/qiniu/support/AuthMain.java
+++ springworld/src/main/java/qiniu/support/AuthMain.java
@@ -3,7 +3,6 @@ package qiniu.support;
 import com.qiniu.util.Auth;
 
 import java.util.HashMap;
-
 /**
  * Created by zhuoxiuwu on 2017/3/9.
  */
@@ -16,11 +15,9 @@ public class AuthMain {
         String scopePdf = qiniuConfig.get("Scope-pdf");
         System.out.println(accessKey + ":" + secretKey);
         System.out.print("scopePdf dir is "+scopePdf);
-
         Auth auth = Auth.create(accessKey, secretKey);
         String upToken = auth.uploadToken(scopePdf);
         System.out.println("uptoken is "+upToken);
-
 
     }
 }
